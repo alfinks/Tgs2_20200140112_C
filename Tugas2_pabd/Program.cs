8aa5dc523eae8a1c22b71fb8645a6001fee23e3d
alfinks <maulanaafiansyah@gmail.com>
2022-03-26 02:22:03 +0700

    Memasukka isi Tabel Transaksi

--- Tugas2_pabd/Program.cs
+++ Tugas2_pabd/Program.cs
@@ -22,10 +22,21 @@ namespace Tugas2_pabd
                 con.Open();
 
                 SqlCommand cm = new SqlCommand("insert into kasir(Id_kasir, Nama_Kasir, Bagian) values('2020001', 'Maulana', 'Pagi')"
-                   + "insert into kasir(Id_kasir, Nama_Kasir, Bagian) values('2020001', 'Maulana', 'Sore')"
+                   + "insert into kasir(Id_kasir, Nama_Kasir, Bagian) values('2020001', 'Maulanaa', 'Sore')"
                    + "insert into kasir(Id_kasir, Nama_Kasir, Bagian) values('20200012', 'Alfiansyah', 'Siang')"
                    + "insert into kasir(Id_kasir, Nama_Kasir, Bagian) values('20200013', 'Alfin', 'Malam')"
-                   + "insert into kasir(Id_kasir, Nama_Kasir, Bagian) values('20200014', 'Maul', 'Pagi')", con);
+                   + "insert into kasir(Id_kasir, Nama_Kasir, Bagian) values('20200014', 'Maul', 'Pagi')"
+
+
+                   +"insert into Transaksi(id_Transaksi, id_Barang, id_Costumer, id_kasir,Nama_Kasir,Tgl_Transaksi) values('T001','B001','C001', 'K001','Maulana','26-03-2021') "
+                   +"insert into Transaksi(id_Transaksi, id_Barang, id_Costumer, id_kasir,Nama_Kasir,Tgl_Transaksi) values('T002','B002','C002', 'K002','Alfiansyah','27-03-2021') "
+                   +"insert into Transaksi(id_Transaksi, id_Barang, id_Costumer, id_kasir,Nama_Kasir,Tgl_Transaksi) values('T003','B003','C003', 'K003','Alfin','28-03-2021') "
+                   +"insert into Transaksi(id_Transaksi, id_Barang, id_Costumer, id_kasir,Nama_Kasir,Tgl_Transaksi) values('T004','B004','C004', 'K004','Maul','29-03-2021') "
+                   +"insert into Transaksi(id_Transaksi, id_Barang, id_Costumer, id_kasir,Nama_Kasir,Tgl_Transaksi) values('T005','B005','C005', 'K005','alfian','30-03-2021') "
+
+                   , 
+                   
+                   con);
 
                 cm.ExecuteNonQuery();
 
