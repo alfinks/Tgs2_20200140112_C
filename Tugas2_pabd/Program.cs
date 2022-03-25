c072f856e44197f1ea2a0751101ed845e95d3fc2
alfinks <maulanaafiansyah@gmail.com>
2022-03-26 01:33:14 +0700

    Add project files.

---
+++ Tugas2_pabd/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Tugas2_pabd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().InsertData();
+        }
+        public void InsertData()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=ALFIN\\ALFINKS; database=Tugas2; Integrated Security = TRUE");
+                con.Open();
+
+
+            }
+        }
+        
+        
+    }
+}
